5a157c4240bb1c006cdf13041c28d579198ea446
yvpaulo <yvpaulo@yahoo.com.br>
2020-11-02 11:01:20 -0300

    Adicionar um protocolo de mensagem de controle da Internet(ICMP)

---
+++ ICMPHealthCheck.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheck : IHealthCheck {
+    public class ICMPHealthCheck {
+        private string Host = "www.does-not-exist.com";
+        private int Timeout = 300;
+        public async Task<HealthCheckResult> CheckHealthAsync (
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default) {
+            try {
+                using (var ping = new Ping ()) {
+                    var reply = await ping.SendPingAsync (Host);
+                    switch (reply.Status) {
+                        case IPStatus.Success:
+                            return (reply.RoundtripTime > Timeout) ?
+                                HealthCheckResult.Degraded () :
+                                HealthCheckResult.Healthy ();
+                        default:
+                            return HealthCheckResult.Unhealthy ();
+                    }
+                }
+            } catch (Exception e) {
+                return HealthCheckResult.Unhealthy ();
+            }
+        }
+
+    }
+}
