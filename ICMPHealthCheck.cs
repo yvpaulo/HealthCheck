2e95ee71fe0026843475c987757d0ffea8288b4d
yvpaulo@yahoo.com.br <yvpaulo@yahoo.com.br>
2020-11-02 13:12:41 -0300

    CustomHealthCheckOptions

--- ICMPHealthCheck.cs
+++ ICMPHealthCheck.cs
@@ -1,31 +1,60 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
 using System.Net.NetworkInformation;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace HealthCheck
 {
-    public class ICMPHealthCheck : IHealthCheck {
-        private string Host = "www.does-not-exist.com";
-        private int Timeout = 300;
-        public async Task<HealthCheckResult> CheckHealthAsync (
+    public class ICMPHealthCheck : IHealthCheck
+    {
+        private string Host { get; set; }
+        private int Timeout { get; set; }
+
+        public ICMPHealthCheck(string host, int timeout)
+        {
+            Host = host;
+            Timeout = timeout;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
             HealthCheckContext context,
-            CancellationToken cancellationToken = default) {
-            try {
-                using (var ping = new Ping ()) {
-                    var reply = await ping.SendPingAsync (Host);
-                    switch (reply.Status) {
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using (var ping = new Ping())
+                {
+                    var reply = await ping.SendPingAsync(Host);
+
+                    switch (reply.Status)
+                    {
                         case IPStatus.Success:
-                            return (reply.RoundtripTime > Timeout) ?
-                                HealthCheckResult.Degraded () :
-                                HealthCheckResult.Healthy ();
+                            var msg = String.Format(
+                                "IMCP to {0} took {1} ms.",
+                                Host,
+                                reply.RoundtripTime);
+
+                            return (reply.RoundtripTime > Timeout)
+                                ? HealthCheckResult.Degraded(msg)
+                                : HealthCheckResult.Healthy(msg);
+
                         default:
-                            return HealthCheckResult.Unhealthy ();
+                            var err = String.Format(
+                                "IMCP to {0} failed: {1}",
+                                Host,
+                                reply.Status);
+                            return HealthCheckResult.Unhealthy(err);
                     }
                 }
-            } catch (Exception e) {
-                return HealthCheckResult.Unhealthy ();
+            }
+            catch (Exception e)
+            {
+                var err = String.Format(
+                    "IMCP to {0} failed: {1}",
+                    Host,
+                    e.Message);
+                return HealthCheckResult.Unhealthy(err);
             }
         }
     }
